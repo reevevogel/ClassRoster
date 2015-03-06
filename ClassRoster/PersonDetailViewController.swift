516ac7c401c9f5834b6b0a08802020a7a33a203c
Reeve Vogel <reeve.vogel@yahoo.com>
2015-03-05 18:59:27 -0800

    updated all files to current -day 5- content

--- ClassRoster/PersonDetailViewController.swift
+++ ClassRoster/PersonDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  PersonDetailViewController.swift
+//  PersonDeatilViewController.swift
 //  ClassRoster
 //
 //  Created by Reeve Vogel on 2/26/15.
@@ -8,28 +8,50 @@
 
 import UIKit
 
-class PersonDetailViewController: UIViewController {
-
+class PersonDetailViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    var selectedPerson = Person(firstName: "Dummy", lastName: "Dummy")
+    
+    
+    @IBOutlet weak var fName: UILabel!
+    
+    @IBOutlet weak var lName: UILabel!
+    
+    @IBOutlet weak var fNameText: UITextField!
+    
+    @IBOutlet weak var lNameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.title = self.selectedPerson.firstName
+        
+        self.fNameText.delegate = self
+        self.lNameText.delegate = self
+        
+        fName.text = "First: \(self.selectedPerson.firstName)"
+        lName.text = "Last: \(self.selectedPerson.lastName)"
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
     
-
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
